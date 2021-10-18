97eec891104030a0352e2ca7b27730192c43b12c
AhmadAttalhaoui <61194391+AhmadAttalhaoui@users.noreply.github.com>
2021-10-18 22:18:01 +0200

    een verbetering

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,71 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbTijd = new System.Windows.Forms.Label();
+            this.lbInfo = new System.Windows.Forms.Label();
             this.lbTitel = new System.Windows.Forms.Label();
-            this.lbInformatie = new System.Windows.Forms.Label();
-            this.tbTijd = new System.Windows.Forms.TextBox();
+            this.tbInfo = new System.Windows.Forms.TextBox();
             this.tbTitel = new System.Windows.Forms.TextBox();
-            this.tbInformatie = new System.Windows.Forms.TextBox();
             this.cbTijd = new System.Windows.Forms.CheckBox();
             this.btOpslaan = new System.Windows.Forms.Button();
             this.btVolgendeTaak = new System.Windows.Forms.Button();
             this.btVerwijderTaak = new System.Windows.Forms.Button();
             this.btZetAchteraan = new System.Windows.Forms.Button();
+            this.btMessage = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lbTijd
+            // lbInfo
             // 
-            this.lbTijd.AutoSize = true;
-            this.lbTijd.Location = new System.Drawing.Point(54, 96);
-            this.lbTijd.Name = "lbTijd";
-            this.lbTijd.Size = new System.Drawing.Size(26, 15);
-            this.lbTijd.TabIndex = 0;
-            this.lbTijd.Text = "Tijd";
+            this.lbInfo.AutoSize = true;
+            this.lbInfo.Location = new System.Drawing.Point(54, 171);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(62, 15);
+            this.lbInfo.TabIndex = 0;
+            this.lbInfo.Text = "Informatie";
             // 
             // lbTitel
             // 
             this.lbTitel.AutoSize = true;
-            this.lbTitel.Location = new System.Drawing.Point(202, 96);
+            this.lbTitel.Location = new System.Drawing.Point(54, 58);
             this.lbTitel.Name = "lbTitel";
             this.lbTitel.Size = new System.Drawing.Size(29, 15);
             this.lbTitel.TabIndex = 1;
             this.lbTitel.Text = "Titel";
             // 
-            // lbInformatie
+            // tbInfo
             // 
-            this.lbInformatie.AutoSize = true;
-            this.lbInformatie.Location = new System.Drawing.Point(350, 96);
-            this.lbInformatie.Name = "lbInformatie";
-            this.lbInformatie.Size = new System.Drawing.Size(62, 15);
-            this.lbInformatie.TabIndex = 2;
-            this.lbInformatie.Text = "Informatie";
-            // 
-            // tbTijd
-            // 
-            this.tbTijd.Location = new System.Drawing.Point(26, 126);
-            this.tbTijd.Name = "tbTijd";
-            this.tbTijd.Size = new System.Drawing.Size(100, 23);
-            this.tbTijd.TabIndex = 3;
+            this.tbInfo.Location = new System.Drawing.Point(54, 189);
+            this.tbInfo.Multiline = true;
+            this.tbInfo.Name = "tbInfo";
+            this.tbInfo.Size = new System.Drawing.Size(366, 186);
+            this.tbInfo.TabIndex = 3;
             // 
             // tbTitel
             // 
-            this.tbTitel.Location = new System.Drawing.Point(175, 126);
+            this.tbTitel.Location = new System.Drawing.Point(54, 76);
             this.tbTitel.Name = "tbTitel";
-            this.tbTitel.Size = new System.Drawing.Size(100, 23);
+            this.tbTitel.Size = new System.Drawing.Size(270, 23);
             this.tbTitel.TabIndex = 4;
-            // 
-            // tbInformatie
-            // 
-            this.tbInformatie.Location = new System.Drawing.Point(320, 126);
-            this.tbInformatie.Name = "tbInformatie";
-            this.tbInformatie.Size = new System.Drawing.Size(100, 23);
-            this.tbInformatie.TabIndex = 5;
             // 
             // cbTijd
             // 
             this.cbTijd.AutoSize = true;
-            this.cbTijd.Location = new System.Drawing.Point(454, 130);
+            this.cbTijd.Location = new System.Drawing.Point(54, 131);
             this.cbTijd.Name = "cbTijd";
             this.cbTijd.Size = new System.Drawing.Size(45, 19);
             this.cbTijd.TabIndex = 6;
@@ -101,25 +86,25 @@
             // 
             // btOpslaan
             // 
-            this.btOpslaan.Location = new System.Drawing.Point(202, 199);
+            this.btOpslaan.Location = new System.Drawing.Point(54, 402);
             this.btOpslaan.Name = "btOpslaan";
-            this.btOpslaan.Size = new System.Drawing.Size(75, 23);
+            this.btOpslaan.Size = new System.Drawing.Size(154, 36);
             this.btOpslaan.TabIndex = 7;
             this.btOpslaan.Text = "Opslaan";
             this.btOpslaan.UseVisualStyleBackColor = true;
             // 
             // btVolgendeTaak
             // 
-            this.btVolgendeTaak.Location = new System.Drawing.Point(28, 264);
+            this.btVolgendeTaak.Location = new System.Drawing.Point(558, 210);
             this.btVolgendeTaak.Name = "btVolgendeTaak";
-            this.btVolgendeTaak.Size = new System.Drawing.Size(98, 23);
+            this.btVolgendeTaak.Size = new System.Drawing.Size(100, 23);
             this.btVolgendeTaak.TabIndex = 8;
             this.btVolgendeTaak.Text = "Volgende Taak";
             this.btVolgendeTaak.UseVisualStyleBackColor = true;
             // 
             // btVerwijderTaak
             // 
-            this.btVerwijderTaak.Location = new System.Drawing.Point(175, 264);
+            this.btVerwijderTaak.Location = new System.Drawing.Point(558, 145);
             this.btVerwijderTaak.Name = "btVerwijderTaak";
             this.btVerwijderTaak.Size = new System.Drawing.Size(100, 23);
             this.btVerwijderTaak.TabIndex = 9;
@@ -128,7 +113,7 @@
             // 
             // btZetAchteraan
             // 
-            this.btZetAchteraan.Location = new System.Drawing.Point(320, 264);
+            this.btZetAchteraan.Location = new System.Drawing.Point(558, 89);
             this.btZetAchteraan.Name = "btZetAchteraan";
             this.btZetAchteraan.Size = new System.Drawing.Size(100, 23);
             this.btZetAchteraan.TabIndex = 10;
@@ -136,22 +121,40 @@
             this.btZetAchteraan.UseVisualStyleBackColor = true;
             this.btZetAchteraan.Click += new System.EventHandler(this.btZetAchteraan_Click);
             // 
+            // btMessage
+            // 
+            this.btMessage.Location = new System.Drawing.Point(558, 282);
+            this.btMessage.Name = "btMessage";
+            this.btMessage.Size = new System.Drawing.Size(100, 47);
+            this.btMessage.TabIndex = 11;
+            this.btMessage.Text = "Delen Message";
+            this.btMessage.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(558, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 53);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btMessage);
             this.Controls.Add(this.btZetAchteraan);
             this.Controls.Add(this.btVerwijderTaak);
             this.Controls.Add(this.btVolgendeTaak);
             this.Controls.Add(this.btOpslaan);
             this.Controls.Add(this.cbTijd);
-            this.Controls.Add(this.tbInformatie);
             this.Controls.Add(this.tbTitel);
-            this.Controls.Add(this.tbTijd);
-            this.Controls.Add(this.lbInformatie);
+            this.Controls.Add(this.tbInfo);
             this.Controls.Add(this.lbTitel);
-            this.Controls.Add(this.lbTijd);
+            this.Controls.Add(this.lbInfo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -161,17 +164,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lbTijd;
+        private System.Windows.Forms.Label lbInfo;
         private System.Windows.Forms.Label lbTitel;
-        private System.Windows.Forms.Label lbInformatie;
-        private System.Windows.Forms.TextBox tbTijd;
+        private System.Windows.Forms.TextBox tbInfo;
         private System.Windows.Forms.TextBox tbTitel;
-        private System.Windows.Forms.TextBox tbInformatie;
         private System.Windows.Forms.CheckBox cbTijd;
         private System.Windows.Forms.Button btOpslaan;
         private System.Windows.Forms.Button btVolgendeTaak;
         private System.Windows.Forms.Button btVerwijderTaak;
         private System.Windows.Forms.Button btZetAchteraan;
+        private System.Windows.Forms.Button btMessage;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ TeDoen.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht4
+{
+    internal class TeDoen<T>
+    {
+        public static int Id { get; set; }
+        public static int counter = 0;
+        public string Title { get; set; }
+        public string Description {  get; set; }
+
+        public TeDoen(string title, string description)
+        {
+            counter = Id;
+            Title = title;
+            counter++;
+            Description = description;
+        }
+
+        List<T> lijst = new List<T>();
+
+        public void addLijst(T add)
+        {
+            lijst.Add(add);
+        }
+
+        public T RemoveLijst()
+        {
+            if (lijst.Count == 0)
+                throw new StackOverflowException();
+            T del = lijst[0];
+            lijst.RemoveAt(0);
+            return del;
+        }
+
+        public T Toon(T iets)
+        {
+            if (lijst.Contains(iets))
+            {
+                return iets;
+            }
+            else
+            {
+                return lijst[0];
+            }
+        }
+
+        public void Delete(T deleteAll)
+        {
+            lijst.Clear();
+        }
+
+        public override string? ToString()
+        {
+            return String.Join(",", lijst);
+        }
+
+        public bool isAanwezig(T iets)
+        {
+            if (lijst.Contains(iets))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<T>? Copy(List<T> stapel)
+        {
+            List<T> Tweedelijst = new List<T>();
+            foreach (T add in stapel)
+            {
+                Tweedelijst.Add(add);
+            }
+            return Tweedelijst;
+        }
+    }
+
+    }
+
